f30b10abd9769fd90c72002de249bacbc0474591
RaquelArguedas <raquelarguedas@estudiantec.cr>
2024-09-06 11:47:43 +0200

    Larger dataset addded

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
-import umap
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+import h3
 import os
 
 # Goes throw downloaded datasets to make one. Returns that one last dataset.
@@ -12,7 +12,7 @@ def get_datasets():
     df = pd.DataFrame()
     multi_df = pd.DataFrame()
     for name in os.listdir('C:/Users/raque/OneDrive - Estudiantes ITCR/Raquel/TEC/2024 II Semestre/CoCoProject/specie-location-clustering/originalDatasets'):
-        temp_df = pd.read_csv(f'originalDatasets/{name}', delimiter='\t')
+        temp_df = pd.read_csv(f'originalDatasets/{name}', delimiter='\t', low_memory=False)
         if name.startswith('occurrence'):
             df = pd.concat([df, temp_df], ignore_index=True)
         else:
@@ -26,13 +26,6 @@ def get_columns_info(df):
         column_type = df[column].dtype
         null_count = df[column].isnull().sum()
         print(f"Column Name: {column_name}, Type: {column_type}, Null Counts: {null_count} of {len(df)}")
-
-# Returns true or false if there is a full null column
-def any_null_column(df):
-    for column in df.columns:
-        if (df[column].isnull().sum() == len(df)):
-            return True
-    return False
 
 # Returns true or false if there is a full null column
 def any_null_column(df):
@@ -72,12 +65,21 @@ def add_multi(df, multi_df):
     multi_dict = get_multi_dict(multi_df)
 
     for i in range(len(ids)):
+        print (f"{i} out of {len(ids)}", end="\r", flush=True)
         for item in multi_dict:
             if (item[0] == ids[i]):
                 col[i] = item
 
     df['identifier'] = col;
 
+# Add hexagon columns
+def add_hexagons(df):
+    col = []
+    for index, item in df.iterrows():
+        print (f"{index} out of {len(df)}", end="\r")
+        cell = h3.latlng_to_cell(item['decimalLatitude'], item['decimalLongitude'], 1)
+        col += [cell]
+    df['hexagon'] = col
 
 def unique_values(list):
     result = []
@@ -98,7 +100,7 @@ def get_correlation_table(df, header):
     # fill the table
     for country_index in range(1, len(table[0])):
         for specie_index in range(1, len(table)):
-            temp = df[(df['countryCode']==table[0][country_index]) & (df['scientificName']==table[specie_index][0])]
+            temp = df[(df['hexagon']==table[0][country_index]) & (df['scientificName']==table[specie_index][0])]
             table[specie_index][country_index] = len(temp)
     
     return table
@@ -106,7 +108,7 @@ def get_correlation_table(df, header):
 def standarize(df):
     columns = df.columns[1:]
     df = df[columns]
-    df = StandardScaler().fit_transform(df)
+    df = MinMaxScaler().fit_transform(df[columns])
     df = pd.DataFrame(data=df, columns=columns)
     return df
 
@@ -114,6 +116,7 @@ def get_best_k(df, columns):
     bestK = 2
     maxSil = -2
     for k in range(2, 11):
+        print('k: ', k)
         kmeans = KMeans(n_clusters = k).fit(df[columns])
         labels = kmeans.labels_
         result = silhouette_score(df[columns], labels, metric = 'euclidean')
@@ -126,8 +129,11 @@ def do_clustering(df):
     columns = df.columns[1:]
     df = df[columns]
     k = get_best_k(df, columns)
+    print('After k')
     km = KMeans(n_clusters=k) 
+    print('Clustering done')
     y2 = km.fit_predict(df[columns]) 
+    print('Row done')
     return y2
 
 # visualize
@@ -138,7 +144,9 @@ def visualize(df):
     plt.show()
 
 def main():
+    print('Starting...')
     df, multi_df = get_datasets()
+    print('Datasets obtained')
 
     # reduce to desired columns
     multi_df = multi_df[['gbifID', 'identifier']]
@@ -148,17 +156,36 @@ def main():
             'scientificName', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'genericName', 
             'specificEpithet', 'taxonRank']]
 
+    # not null values in those columns
+    df = df[df['decimalLatitude'].notnull() & df['decimalLongitude'].notnull() & df['scientificName'].notnull()]
 
+    print('Columns and rows reduced, dataset info:', df.info())
+
+    print('Adding multi column...')
     add_multi(df, multi_df)
-    correlation_columns = unique_values(['scientificName'] + df['countryCode'].tolist())
+    print('Adding hexagons column...')
+    add_hexagons(df)
+
+    print('Other columns added')
+
+    correlation_columns = unique_values(['scientificName'] + df['hexagon'].tolist())
     correlation_df = pd.DataFrame(get_correlation_table(df, correlation_columns)[1:])
     correlation_df.columns = correlation_columns
     # print(correlation_df) # table with occurences region specie relation
+    print('Table done, creating csv...')
+    correlation_df.to_csv(os.path.join(os.getcwd(), 'correlation_table.csv'))
 
     correlation_df = standarize(correlation_df)
-    print(correlation_df) # standarize table
+    # print(correlation_df) # standarize table
+    print('Creating second csv...')
+    correlation_df.to_csv(os.path.join(os.getcwd(), 'normalize_correlation_table.csv'))
 
-    correlation_df['cluster'] = do_clustering(correlation_df)
+
+    # Need to debug
+    # print('Doing cluster...')
+    # correlation_df['cluster'] = do_clustering(correlation_df)
     # print(correlation_df.head())
+
+    print('Done')
     
 main()
