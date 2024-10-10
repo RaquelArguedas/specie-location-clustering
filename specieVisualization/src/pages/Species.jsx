d0a459b5fa548e98c199665213c6358660ccce0b
RaquelArguedas <raquelarguedas@estudiantec.cr>
2024-10-10 10:28:39 +0200

    Circles shadow on the back

--- specieVisualization/src/pages/Species.jsx
+++ specieVisualization/src/pages/Species.jsx
@@ -26,12 +26,12 @@ const Species = () => {
   };
   
   const updateChart = useCallback(async (type, paramsAPI) => {
-    setBestK(1)
-    setActualCluster(type)
-
-    if (paramsAPI == undefined) paramsAPI = {} 
+    setBestK(1);
+    setActualCluster(type);
+  
+    if (paramsAPI == undefined) paramsAPI = {}; 
     if (!svgRef.current) return;
-
+  
     setLoading(true);
     const body = JSON.stringify({ type, paramsAPI });
     const res = await fetch(`http://127.0.0.1:5000/do_cluster`, {
@@ -42,28 +42,42 @@ const Species = () => {
       body: body,
     });
     const response = await res.json();
-    let data = response['cluster']
-    setBestK(response['bestK'])
+    let data = response['cluster'];
+    setBestK(response['bestK']);
     setLoading(false);
-
+  
     const svg = d3.select(svgRef.current);
-
     const svgWidth = svgRef.current.clientWidth;
     const svgHeight = svgRef.current.clientHeight;
-    let size = svgWidth>svgHeight ? svgHeight : svgWidth;
-    size = (size - Math.floor(size/6))
-
+    let size = svgWidth > svgHeight ? svgHeight : svgWidth;
+    size = size - Math.floor(size / 6);
+  
     const xScale = d3.scaleLinear()
       .domain([d3.min(data, d => d.UMAP1), d3.max(data, d => d.UMAP1)])
-      .range([(svgWidth/2)-(size/2), (svgWidth/2)+(size/2)]);
-
+      .range([(svgWidth / 2) - (size / 2), (svgWidth / 2) + (size / 2)]);
+  
     const yScale = d3.scaleLinear()
       .domain([d3.min(data, d => d.UMAP2), d3.max(data, d => d.UMAP2)])
-      .range([(svgHeight/2)+(size/2), (svgHeight/2)-(size/2)]);
+      .range([(svgHeight / 2) + (size / 2), (svgHeight / 2) - (size / 2)]);
+  
+    svg.selectAll("g").remove();
+    
+    const shadowGroup = svg.append("g").attr("class", "shadow-group");
+    const circleGroup = svg.append("g").attr("class", "circle-group");
+  
+    shadowGroup.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xScale(d.UMAP1))
+      .attr("cy", d => yScale(d.UMAP2)) 
+      .attr("r", d => getSize(d.family))
+      .attr("fill", d => colorScale(d.cluster))
+      .style("filter", d => 
+        `drop-shadow(0px 0px 30px ${colorScale(d.cluster)}) drop-shadow(0px 0px 15px ${colorScale(d.cluster)})`)
+      .lower();  
 
-    svg.selectAll("circle").remove();
-
-    svg.selectAll("circle")
+    circleGroup.selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
@@ -73,7 +87,7 @@ const Species = () => {
       .attr("fill", d => {
         const patternId = `pattern-${d.identifier[0].replace(/[^a-zA-Z0-9-_]/g, '')}`;
         const circleRadius = getSize(d.family);
-
+  
         if (svg.select(`#${patternId}`).empty()) {
           svg.append("defs")
             .append("pattern")
@@ -88,86 +102,44 @@ const Species = () => {
             .attr("preserveAspectRatio", "xMidYMid slice")
             .attr("filter", "grayscale(100%)"); 
         }        
-
         return `url(#${patternId})`;
       })
       .attr("stroke", d => colorScale(d.cluster))
       .attr("stroke-width", 2)
-      .style("filter", d => 
-        `drop-shadow(0px 0px 30px ${colorScale(d.cluster)}) drop-shadow(0px 0px 15px ${colorScale(d.cluster)})`
-      )
       .on("click", (event, d) => {
         setJsonInfo(d);
+        shadowGroup.lower(); 
       })
       .on("mouseover", function(event, d) {
         const patternId = `pattern-${d.identifier[0].replace(/[^a-zA-Z0-9-_]/g, '')}`;
         const circleRadius = 1.2 * d3.select(this).attr("r");
         d3.select(this)
           .attr("r", circleRadius) 
-          .raise();       
+          .raise(); 
+  
         svg.select(`#${patternId} image`)
           .attr("filter", "none")
           .attr("width", circleRadius * 2) 
-          .attr("height", circleRadius * 2);    
+          .attr("height", circleRadius * 2);
+  
+        shadowGroup.lower(); 
       })
       .on("mouseout", function(event, d) {
         const patternId = `pattern-${d.identifier[0].replace(/[^a-zA-Z0-9-_]/g, '')}`;
         const circleRadius = d3.select(this).attr("r") / 1.2;
         d3.select(this)
           .attr("r", circleRadius)
-          .lower();    
+          .lower();
         svg.select(`#${patternId} image`)
           .attr("filter", "grayscale(100%)")
           .attr("width", circleRadius * 2) 
-          .attr("height", circleRadius * 2); 
+          .attr("height", circleRadius * 2);
+  
+        shadowGroup.lower();  
       });
-
-
-    const zoom = d3.zoom()
-      .scaleExtent([0.5, 40])
-      .on('zoom', zoomed);
-
-    function zoomed(event) {
-      const transform = event.transform;
-    
-      const newXScale = transform.rescaleX(xScale);
-      const newYScale = transform.rescaleY(yScale);
-
-      svg.selectAll("circle")
-        .attr("cx", d => newXScale(d.UMAP1))
-        .attr("cy", d => newYScale(d.UMAP2));
-
-      if (transform.k < 10) {
-        svg.selectAll("circle")
-        .attr("r", d => getSize(d.family) * transform.k)
-        .attr("fill", d => {
-          const patternId = `pattern-${d.identifier[0].replace(/[^a-zA-Z0-9-_]/g, '')}`;
-          const circleRadius = getSize(d.family) * transform.k;
-    
-          if (svg.select(`#${patternId}`).empty()) {
-            svg.append("defs")
-              .append("pattern")
-              .attr("id", patternId)
-              .attr("patternUnits", "objectBoundingBox")
-              .attr("width", 1)
-              .attr("height", 1)
-              .append("image")
-              .attr("xlink:href", d.identifier[0])
-              .attr("width", circleRadius * 2) 
-              .attr("height", circleRadius * 2)
-              .attr("preserveAspectRatio", "xMidYMid slice");
-          } else {
-            svg.select(`#${patternId} image`)
-              .attr("width", circleRadius * 2)
-              .attr("height", circleRadius * 2);
-          }
-          return `url(#${patternId})`;
-        });
-      }
-    };
-    
-    svg.call(zoom);
+  
   }, []);
+  
 
   const sendParams = (params) => {
     updateChart(actualCluster, params)
